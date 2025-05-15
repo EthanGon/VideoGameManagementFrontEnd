708ffa7d62e87fcfceeb2b946d6c8816e38d61f2
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 19:08:33 -0400

    can make a request to api to add a game and update the status of a game

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -5,3 +5,29 @@ export const searchGames = (query) => {
     method: "GET",
   });
 };
+
+export const getGame = (id) => {
+  return fetch(`${API_URL}/games/${id}`, {
+    method: "GET",
+  });
+};
+
+export const addGame = (game) => {
+  return fetch(`${API_URL}/games`, {
+    method: "POST",
+    body: JSON.stringify(game),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+export const updateGame = (id, updatedStatus) => {
+  return fetch(`${API_URL}/games/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(updatedStatus),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
