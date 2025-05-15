1ba39c8d5dfde3af26c1811defb6d4d2a1a252bf
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 16:32:48 -0400

    responsive header that links to home, games and search pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold underline">Header</h1>
-    </div>
+    <header className="bg-neutral-900 text-slate-100">
+      <div className="mx-auto flex max-w-6xl items-center justify-between p-4">
+        <Link to="/" className="text-2xl font-bold md:text-4xl">
+          Home
+        </Link>
+        <nav className="space-x-6 text-lg font-medium md:text-2xl">
+          <Link to="/games" className="hover:opacity-90">
+            Games
+          </Link>
+          <Link to="/search" className="hover:opacity-90">
+            Search
+          </Link>
+        </nav>
+      </div>
+    </header>
   );
 };
 
