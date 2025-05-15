f215aede92c793209074cbc622c220b343d1b5f2
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 17:07:12 -0400

    search page gets the query that the user searches for and if that query doesnt exist in the location state then a empty string is used instead

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 const Search = () => {
+  const location = useLocation();
+  const query = location.state?.query || "";
+
   return (
     <div>
-      <h1 className="text-4xl font-bold underline">Search Page</h1>
+      <h1 className="text-4xl font-bold underline">
+        {0} results for {query}
+      </h1>
     </div>
   );
 };
