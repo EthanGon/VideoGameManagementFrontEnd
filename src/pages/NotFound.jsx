f09395974f32b2bd145a43016b6db6f4c6d2a5ea
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 19:49:04 -0400

    page for when user trys to go to link that does not exist

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold underline">NotFound Page</h1>
-    </div>
+    <section className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-4 p-4 py-20 text-center text-slate-100">
+      <h1 className="text-center text-4xl font-bold text-blue-900">
+        The page you are looking for does not exist.
+      </h1>
+      <Link
+        to="/"
+        className="rounded-md bg-blue-900 px-4 py-3 text-xl font-bold text-white hover:opacity-90"
+      >
+        Back To Home
+      </Link>
+    </section>
   );
 };
 
