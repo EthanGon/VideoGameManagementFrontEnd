9eb949fe93174adf6876f47a89f227a441bee283
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 16:03:39 -0400

    used react router to add routing between every page in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import GamesList from "./pages/GamesList";
+import GameDetails from "./pages/GameDetails";
+import NotFound from "./pages/NotFound";
+
+// layouts
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold underline">Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="games" element={<GamesList />} />
+          <Route path="games/:id" element={<GameDetails />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
