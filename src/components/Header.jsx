9b7041b91ad8593a82651b8fe3a7826ad7ec7ae4
Ethan Gonzalez <ethangonzalez003@gmail.com>
2025-05-15 17:08:46 -0400

    forgot to add hover opacity to home link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <header className="bg-neutral-900 text-slate-100">
       <div className="mx-auto flex max-w-6xl flex-col items-start justify-between gap-4 p-4 md:flex-row md:items-center md:gap-0">
-        <Link to="/" className="text-2xl font-bold md:text-4xl">
+        <Link
+          to="/"
+          className="text-2xl font-bold hover:opacity-90 md:text-4xl"
+        >
           Home
         </Link>
         <nav className="flex w-full flex-col items-start gap-4 text-lg font-medium md:w-auto md:flex-row md:items-center md:gap-0 md:space-x-6 md:text-2xl">
